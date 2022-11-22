7811eb7c0e8eb7331fd1feb87ed73fb796355d91
hsh9588 <hsh9588@naver.com>
2022-11-22 18:00:21 +0900

    20221122 비동기 테이블 작성중

--- travelCompass/src/main/webapp/resources/js/profile/MemberPage.js
+++ travelCompass/src/main/webapp/resources/js/profile/MemberPage.js
@@ -69,7 +69,7 @@ for(let item of suportButton){
 
 }
 
-// 비동기 리뷰 테이블 작성
+// 비동기 리뷰 테이블 작성 (사진 없는 테이블)
 
 const reviewTextColum = document.createElement("div");
 reviewTextColum.classList.add("user-page-review-colums2");
@@ -80,13 +80,13 @@ reviewTextColum.classList.add("user-page-review-colums2");
         const reviewTextHeaderLayout = document.createElement("div");
         reviewTextHeaderLayout.classList.add("user-page-review-header-layout");
 
-            const reviewTextImage = document.createElement("a");
-            reviewTextImage.classList.add("review-user-image");
+            const reviewTextUserImage = document.createElement("a");
+            reviewTextUserImage.classList.add("review-user-image");
 
-            reviewTextImage.innerHTML = "<img src='${loginMember.profileImage}'>";
+            reviewTextUserImage.innerHTML = "<img src='${loginMember.profileImage}'>";
 
-            const reviewInfoLayout = document.createElement("div");
-            reviewInfoLayout.classList.add("review-user-info-layout");
+            const reviewTextInfoLayout = document.createElement("div");
+            reviewTextInfoLayout.classList.add("review-user-info-layout");
 
                 const reviewInfoNickname = document.createElement("span");
                 reviewInfoNickname.classList.add("review-user-nickname");
@@ -104,20 +104,20 @@ reviewTextColum.classList.add("user-page-review-colums2");
         const reivewTextDotDownMenu = document.createElement("div");
         reivewTextDotDownMenu.classList.add("user-page-review-dot-down-menu");
 
-            const downMenu = document.createElement("ul");
-            downMenu.classList.add("down-menu");
+            const reivewTextDownMenu = document.createElement("ul");
+            reivewTextDownMenu.classList.add("down-menu");
 
-                const downMenu_li1 = document.createElement("li");
+                const reivewTextDownMenu_li1 = document.createElement("li");
                     
-                    const  downMenu_li1_a = document.createElement("a");
+                    const  reivewTextDownMenu_li1_a = document.createElement("a");
 
-                    downMenu_li1_a.innerText = "수정";
+                    reivewTextDownMenu_li1_a.innerText = "수정";
                     
-                const downMenu_li2 = document.createElement("li");
+                const reivewTextDownMenu_li2 = document.createElement("li");
                     
-                    const  downMenu_li2_a = document.createElement("a");
+                    const  reivewTextDownMenu_li2_a = document.createElement("a");
 
-                    downMenu_li2_a.innerText = "삭제";
+                    reivewTextDownMenu_li2_a.innerText = "삭제";
                     
         const reviewTextDataTableStyle = document.createElement("div");
         reviewTextDataTableStyle.classList.add("review-date-table-style");
@@ -210,7 +210,173 @@ reviewTextColum.classList.add("user-page-review-colums2");
         const reviewTextBottomMenu = document.createElement("div");
         reviewTextBottomMenu.classList.add("review-bottom-menu-style");
 
-            const suportTextButton = document.createElement("div");
-            suportTextButton.classList.add("suport-button");
-            suportTextButton.innerHTML = "<i class='fa-regular fa-thumbs-up'></i>도움이 됨"
+            const reviewTextSuportButton = document.createElement("div");
+            reviewTextSuportButton.classList.add("suport-button");
+            reviewTextSuportButton.innerHTML = "<i class='fa-regular fa-thumbs-up'></i>도움이 됨";
+
+            const  reviewTextSaceButton = document.createElement("div");
+            reviewTextSaceButton.classList.add("save-button");
+            reviewTextSaceButton.innerHTML= "<i class='fa-solid fa-heart'></i>저장";
+
+            const reviewTextShareButton = document.createElement("div");
+            reviewTextShareButton.classList.add("share-button");
+            reviewTextShareButton.innerHTML = "<i class='fa-solid fa-arrow-up-from-bracket'></i>공유";
+
 // --------------------------------------------------------------------------------------------
+
+// 비동기사진 리뷰 테이블 작성
+
+
+const reviewImageColum = document.createElement("div");
+reviewImageColum.classList.add("user-page-review-colums2");
+
+    const reviewImageHeaderStyle = document.createElement("div");
+    reviewImageHeaderStyle.classList.add("user-page-review-header-style");
+
+        const reviewImageHeaderLayout = document.createElement("div");
+        reviewImageHeaderLayout.classList.add("user-page-review-header-layout");
+
+            const reviewImageUserImage = document.createElement("a");
+            reviewImageUserImage.classList.add("review-user-image");
+
+            reviewImageUserImage.innerHTML = "<img src='${loginMember.profileImage}'>";
+
+            const reviewImageInfoLayout = document.createElement("div");
+            reviewImageInfoLayout.classList.add("review-user-info-layout");
+
+                const reviewImageInfoNickname = document.createElement("span");
+                reviewImageInfoNickname.classList.add("review-user-nickname");
+
+                reviewImageInfoNickname.innerHTML = "<a href='#'>유저닉네임</a>님이 리뷰를 작성했습니다."
+
+                    const reviewImageInfoDateLink = document.createElement("a");
+                    reviewImageInfoDateLink.classList.add("review-user-dday");
+
+                    reviewImageInfoDateLink.innerText = "0000년 0월";
+                    
+        const reviewImageDotStyle = document.createElement("div");
+        reviewTextDotStyle.classList.add("user-page-review-dot-style");
+
+        const reivewImageDotDownMenu = document.createElement("div");
+        reivewImageDotDownMenu.classList.add("user-page-review-dot-down-menu");
+
+            const reivewImageDownMenu = document.createElement("ul");
+            reivewImageDownMenu.classList.add("down-menu");
+
+                const reivewImageDownMenu_li1 = document.createElement("li");
+                    
+                    const  reivewImageDownMenu_li1_a = document.createElement("a");
+
+                    reivewImageDownMenu_li1_a.innerText = "수정";
+                    
+                const reivewImageDownMenu_li2 = document.createElement("li");
+                    
+                    const  reivewImageDownMenu_li2_a = document.createElement("a");
+
+                    reivewImageDownMenu_li2_a.innerText = "삭제";
+
+        /* 사진 슬라이드 비동기 테이블 작성 위치 */
+                    
+        const reviewImageDataTableStyle = document.createElement("div");
+        reviewImageDataTableStyle.classList.add("review-date-table-style");
+
+            const reviewImagePoint = document.createElement("div");
+
+                const reviewImage_span1 = document.createElement("span");
+                reviewImage_span1.innerHTML = "<i class='fa-solid fa-circle'></i>";
+
+                const reviewImage_span2 = document.createElement("span");
+                reviewImage_span2.innerHTML = "<i class='fa-solid fa-circle'></i>";
+                
+                const reviewImage_span3 = document.createElement("span");
+                reviewImage_span3.innerHTML = "<i class='fa-solid fa-circle'></i>";
+                
+                const reviewImage_span4 = document.createElement("span");
+                reviewImage_span4.innerHTML = "<i class='fa-solid fa-circle'></i>";
+                
+                const reviewImage_span5 = document.createElement("span");
+                reviewImage_span5.innerHTML = "<i class='fa-solid fa-circle'></i>";
+
+            const reviewImageTitle = document.createElement("div");
+            reviewImageTitle.classList.add("review-title");
+
+            reviewImageTitle.innerText = "리뷰 제목";
+
+            const reviewImageContent = document.createElement("div");
+            reviewImageContent.classList.add("review-content");
+
+            reviewImageContent.innerText = "내용";
+
+            const reviewImageDate = document.createElement("div");
+            reviewImageDate.classList.add("review-date");
+
+                const reviewImage_RDT = document.createElement("span");
+                reviewImage_RDT.classList.add("rdt");
+
+                reviewImage_RDT.innerText = "방문날짜 :";
+
+            reviewImageDate.innerText = "0000년 00월";
+
+        const reviewImageNaviStyle = document.createElement("div");
+        reviewImageNaviStyle.classList.add("review-navi-style");
+
+            const reviewImageNaviLayout = document.createElement("div");
+            reviewImageNaviLayout.classList.add("review-navi-layout");
+
+                const reviewImageNaviImage = document.createElement("a");
+                reviewImageNaviImage.classList.add("review_navi_left");
+                reviewImageNaviImage.innerHTML = "<div><img src='/resources/images/profile/venis.webp'></div>";
+                
+                    const reviewImageNaviTitle = document.createElement("div");
+                    reviewImageNaviTitle.classList.add("review_navi_title");
+                    reviewImageNaviTitle.innerHTML = "<div>위치정보 제목</div>";
+
+                        const reviewImageNaviDeep = document.createElement("div");
+                        reviewImageNaviDeep.classList.add("review-navi-deep");
+
+                            const reviewImageNaviDeepPoint = document.createElement("div");
+                            reviewImageNaviDeepPoint.classList.add("review-navi-deep-point");
+
+                                const reviewImageNavi_span1 = document.createElement("span");
+                                reviewImageNavi_span1.innerHTML = "<i class='fa-solid fa-circle'></i>";
+                
+                                const reviewImageNavi_span2 = document.createElement("span");
+                                reviewImageNavi_span2.innerHTML = "<i class='fa-solid fa-circle'></i>";
+                                
+                                const reviewImageNavi_span3 = document.createElement("span");
+                                reviewImageNavi_span3.innerHTML = "<i class='fa-solid fa-circle'></i>";
+                                
+                                const reviewImageNavi_span4 = document.createElement("span");
+                                reviewImageNavi_span4.innerHTML = "<i class='fa-solid fa-circle'></i>";
+                                
+                                const reviewImageNavi_span5 = document.createElement("span");
+                                reviewImageavi_span5.innerHTML = "<i class='fa-solid fa-circle'></i>";
+
+                            const reviewImageNaviCount = document.createElement("div");
+                            reviewImageNaviCount.innerText = "00건의 리뷰";
+                        
+                        const reviewImageNavi = document.createElement("div");
+                        reviewImageNavi.innerText = "배니스, 이탈리아";
+
+                const reviewImageNaviLike = document.createElement("div");
+                reviewImageNaviLike.classList.add("review-navi-right");
+                reviewImageNaviLike.innerHTML = "<i class='fa-solid fa-heart'></i>";
+
+        const reviewImageSupport = document.createElement("div");
+        reviewImageSupport.classList.add("review-support");
+
+        const reviewImageBottomMenu = document.createElement("div");
+        reviewImageBottomMenu.classList.add("review-bottom-menu-style");
+
+            const reviewImageSuportButton = document.createElement("div");
+            reviewImageSuportButton.classList.add("suport-button");
+            reviewImageSuportButton.innerHTML = "<i class='fa-regular fa-thumbs-up'></i>도움이 됨";
+
+            const  reviewImageSaceButton = document.createElement("div");
+            reviewImageSaceButton.classList.add("save-button");
+            reviewImageSaceButton.innerHTML= "<i class='fa-solid fa-heart'></i>저장";
+
+            const reviewImageShareButton = document.createElement("div");
+            reviewImageShareButton.classList.add("share-button");
+            reviewImageShareButton.innerHTML = "<i class='fa-solid fa-arrow-up-from-bracket'></i>공유";
+
